4df61d598a7ef8a09d259bc8ea0e4944d4f00df3
s1gosain <sidhigosain@gmail.com>
2018-07-11 17:08:20 -0700

    database works

---
+++ server/db/index.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({
+    connectionString: connectionString
+});
+
+module.exports = {
+    query: (text, params, callback) => {
+        return pool.query(text, params, callback)
+    }
+}
